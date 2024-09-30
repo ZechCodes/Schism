6a15f6b06a2b80b8d5603256f11cc5d69a341da7
Zech Zimmerman <hi@zech.codes>
2024-09-30 14:59:09 -0400

    Improve entry point name validation

--- schism/entry_points.py
+++ schism/entry_points.py
@@ -5,11 +5,14 @@ __runtime_entry_points = {}
 
 
 def __validate_name(name: str):
+     if not name.isidentifier():
+         raise RuntimeError(f"Entry point names must be valid Python identifiers: {name!r} (invalid)")
+
      if name.startswith("_"):
          raise RuntimeError(f"Entry point names cannot start with '_': {name!r} (invalid)")
 
-     if not name.isidentifier():
-         raise RuntimeError(f"Entry point names must be valid Python identifiers: {name!r} (invalid)")
+     if name in vars():
+         raise RuntimeError(f"Entry point name is not available: {name!r} (not available)")
 
 
 def create_entry_point(name: str, value: Any):
