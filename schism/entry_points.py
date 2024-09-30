f03ed4fa8b7b60ab3b8d6fd2c43f55cc30017980
Zech Zimmerman <hi@zech.codes>
2024-09-30 14:49:36 -0400

    Implement schism entry points

---
+++ schism/entry_points.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+
+__runtime_entry_points = {}
+
+
+def __validate_name(name: str):
+     if name.startswith("_"):
+         raise RuntimeError(f"Entry point names cannot start with '_': {name!r} (invalid)")
+
+     if not name.isidentifier():
+         raise RuntimeError(f"Entry point names must be valid Python identifiers: {name!r} (invalid)")
+
+
+def create_entry_point(name: str, value: Any):
+    __validate_name(name)
+    __runtime_entry_points[name] = value
+    globals()[name] = value
+
+
+def delete_entry_point(name: str):
+    __validate_name(name)
+    del __runtime_entry_points[name]
+    del globals()[name]
+
+
+def clear_all_entry_points():
+    for name in __runtime_entry_points:
+        del globals()[name]
+
+    __runtime_entry_points.clear()
