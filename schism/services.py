3e1d71f319480ba2f65a4bd426063755464564da
ZechCodes <hi@zech.codes>
2024-10-01 13:20:23 -0400

    Prevent circular import

--- schism/services.py
+++ schism/services.py
@@ -1,7 +1,16 @@
-from schism.controllers import get_controller
+import schism.controllers
 
 
 class Service:
     @classmethod
     def __bevy_constructor__(cls):
-        return get_controller().wiring_strategy.get_facade(cls)
+        controller = schism.controllers.get_controller()
+
+        # Direct injection for service that exist in the running process
+        if controller.is_service_active(cls):
+            return cls()
+
+        # Inject a bridge client to remotely access a service that doesn't exist in the running process
+        else:
+            config = controller.get_service_config(cls)
+            return config.get_bridge_type().create_client(cls)
