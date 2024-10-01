8f11248f7b00c023428a7a3782d9e0c03eeaae90
ZechCodes <hi@zech.codes>
2024-10-01 13:24:09 -0400

    Rewrite the controller

--- schism/controllers.py
+++ schism/controllers.py
@@ -1,32 +1,99 @@
+import os
 import threading
+from typing import Generator, Type, TypeAlias, Callable
 
-from schism.wiring_strategies import BaseWiringStrategy, DirectWiringStrategy
+from bevy import inject, dependency
+from tramp.optionals import Optional
 
+import schism.services as services
+import schism.configs as configs
+
+ServicesConfigMapping: TypeAlias = "dict[Type[services.Service], configs.ServiceConfig]"
 
 _global_controller = None
-_global_controller_write_lock = threading.Lock()
+_controller_write_lock = threading.Lock()
 
 
 class SchismController:
+    ACTIVE_SERVICES = set(os.environ.get("SCHISM_ACTIVE_SERVICES", "").split(","))
+
     def __init__(self):
-        self._wiring_strategy = DirectWiringStrategy()
-        self._wiring_strategy_write_lock = threading.Lock()
+        self._service_configs: "dict[str, configs.ServiceConfig]" = dict(self._load_services_configs())
+        self._active_services: Optional[ServicesConfigMapping] = Optional.Nothing()
+        self._remote_services: Optional[ServicesConfigMapping] = Optional.Nothing()
 
     @property
-    def wiring_strategy(self) -> BaseWiringStrategy:
-        with self._wiring_strategy_write_lock:
-            return self._wiring_strategy
+    def active_services(self) -> ServicesConfigMapping:
+        with _controller_write_lock:
+            match self._active_services:
+                case Optional.Some(active_services):
+                    return active_services
 
-    @wiring_strategy.setter
-    def wiring_strategy(self, value: BaseWiringStrategy):
-        with self._wiring_strategy_write_lock:
-            self._wiring_strategy = value
+                case Optional.Nothing():
+                    self._active_services = Optional.Some(
+                        dict(
+                            self.filter_services(lambda s: s.service in self.ACTIVE_SERVICES)
+                        )
+                    )
+                    return self.active_services
+
+    @property
+    def remote_services(self) -> ServicesConfigMapping:
+        with _controller_write_lock:
+            match self._remote_services:
+                case Optional.Some(remote_services):
+                    return remote_services
+
+                case Optional.Nothing():
+                    self._remote_services = Optional.Some(
+                        dict(
+                            self.filter_services(lambda s: s.service not in self.ACTIVE_SERVICES)
+                        )
+                    )
+                    return self.remote_services
+
+    def filter_services(
+        self, condition: "Callable[[configs.ServiceConfig], bool]"
+    ) -> "Generator[tuple[Type[services.Service], configs.ServiceConfig], None, None]":
+        for service, service_config in self._service_configs.items():
+            if condition(service_config):
+                yield service, service_config
+
+    def find_service_matching(self, service: Type[services.Service]) -> Optional[configs.ServiceConfig]:
+        for s in self._service_configs.values():
+            if issubclass(service, s.get_service_type()):
+                return Optional.Some(s)
+
+        return Optional.Nothing()
+
+    def get_service_config(self, service: Type[services.Service]) -> configs.ServiceConfig:
+        match self.find_service_matching(service):
+            case Optional.Some(service_config):
+                return service_config
+
+            case Optional.Nothing():
+                raise ValueError(f"No service matching {service} is configured.")
+
+    def is_service_active(self, service: Type[services.Service]) -> bool:
+        match self.find_service_matching(service):
+            case Optional.Some(service_config):
+                return service_config.service in self.ACTIVE_SERVICES
+
+            case Optional.Nothing():
+                raise ValueError(f"No service matching {service} is configured.")
+
+    @inject
+    def _load_services_configs(
+            self, config: "configs.ServicesConfig" = dependency()
+    ) -> "Generator[tuple[str, configs.ServiceConfig], None, None]":
+        for service_config in config.services:
+            yield service_config.service, service_config
 
 
 def get_controller() -> SchismController:
     global _global_controller
 
-    with _global_controller_write_lock:
+    with _controller_write_lock:
         if _global_controller is None:
             _global_controller = SchismController()
 
@@ -34,12 +101,12 @@ def get_controller() -> SchismController:
 
 
 def has_controller() -> bool:
-    with _global_controller_write_lock:
+    with _controller_write_lock:
         return _global_controller is not None
 
 
 def set_controller(controller: SchismController):
     global _global_controller
 
-    with _global_controller_write_lock:
+    with _controller_write_lock:
         _global_controller = controller
