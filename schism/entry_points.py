f444bee11f908a2f266434856e88dab75bee0f6c
ZechCodes <hi@zech.codes>
2024-10-03 11:10:10 -0400

    Cleanup entry points module

--- schism/entry_points.py
+++ schism/entry_points.py
@@ -1,10 +1,21 @@
-from schism.controllers import EntryPointController, set_controller
+from schism.controllers import EntryPointController, set_controller, SchismController
 
 
-controller = EntryPointController()
-set_controller(controller)
-controller.bootstrap()
+def setup_controller() -> SchismController:
+    controller = EntryPointController()
+    set_controller(controller)
+    controller.bootstrap()
+    return controller
 
 
-for  name, entry_point in controller.entry_points.items():
-    globals()[name] = entry_point
+def setup_entry_points(controller: SchismController):
+    for name, entry_point in controller.entry_points.items():
+        globals()[name] = entry_point
+
+
+def launch():
+    controller = setup_controller()
+    setup_entry_points(controller)
+
+
+launch()
