5cfa1d3316d5273ad8a54d575d85f150fc7a4fdb
ZechCodes <hi@zech.codes>
2024-10-03 14:41:36 -0400

    Add tests for remote service integration

---
+++ tests/service_test.py
@@ -0,0 +1,52 @@
+from bevy import dependency, inject
+
+from schism.bridges.simple_tcp_bridge import SimpleTCPClient
+from schism.services import Service
+
+
+class ServiceA(Service):
+    def __init__(self):
+        self.value = 0
+
+    async def increment(self):
+        self.value += 1
+
+    async def get_value(self):
+        return self.value
+
+
+class ServiceB(Service):
+    def __init__(self):
+        self.value = 0
+
+    async def increment(self):
+        self.value += 1
+
+    async def get_value(self):
+        return self.value
+
+
+class ServiceC(Service):
+    @inject
+    async def udpate_a(self, a: ServiceA = dependency()):
+        await a.increment()
+
+    @inject
+    async def udpate_b(self, b: ServiceB = dependency()):
+        await b.increment()
+
+    @inject
+    async def get_a(self, a: ServiceA = dependency()):
+        return await a.get_value()
+
+    @inject
+    async def get_b(self, b: ServiceB = dependency()):
+        return await b.get_value()
+
+    @inject
+    def a_is_remote(self, a: ServiceA = dependency()):
+        return isinstance(a, SimpleTCPClient)
+
+    @inject
+    def b_is_remote(self, b: ServiceB = dependency()):
+        return isinstance(b, SimpleTCPClient)
---
+++ tests/test_service_integration.py
@@ -0,0 +1,51 @@
+import asyncio
+
+import pytest
+from bevy import Repository
+
+from schism import controllers
+
+from service_test import ServiceC
+
+
+@pytest.fixture(autouse=True)
+def setup_service_runtime():
+    repo = Repository.factory()
+    Repository.set_repository(repo)
+
+    controller = controllers.EntryPointController()
+    controller._env_active_services = {"services_test.ServiceC"}
+    controllers.set_controller(controller)
+
+
+@pytest.mark.asyncio
+async def test_service_integration():
+    service_a = await asyncio.create_subprocess_shell(
+        'SCHISM_ACTIVE_SERVICES="service_test.ServiceA" python -m schism.entry_points',
+    )
+    service_b = await asyncio.create_subprocess_shell(
+        'SCHISM_ACTIVE_SERVICES="service_test.ServiceB" python -m schism.entry_points',
+    )
+    await asyncio.sleep(1)
+
+    try:
+        service = ServiceC()
+        assert service.a_is_remote()
+        assert service.b_is_remote()
+
+        await service.udpate_a()
+        await service.udpate_b()
+        assert await service.get_a() == 1
+        assert await service.get_b() == 1
+
+        await service.udpate_a()
+        await service.udpate_b()
+        assert await service.get_a() == 2
+        assert await service.get_b() == 2
+
+    finally:
+        service_a.terminate()
+        service_b.terminate()
+        await service_a.wait()
+        await service_b.wait()
+        pass
