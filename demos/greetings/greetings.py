eb4a6e8d30ec4f1ee0784ca0be4c9cfb111466b3
ZechCodes <hi@zech.codes>
2024-10-18 14:17:56 -0400

    Add docs to the greetings demo

--- demos/greetings/greetings.py
+++ demos/greetings/greetings.py
@@ -1,3 +1,17 @@
+"""
+Greeting Demo
+
+When running this script directly it will run as a single, monolithic, application in a single process.
+
+If it is run using the schism command it must be run as two separate processes: the greeting service and the "user"
+facing application. To run the greeting service use the command: `schism run services greeting-service`. Then you can
+run the application using the command: `schism run greetings.main`. You can run that any number of times and it will
+always direct calls to the greeting service class to the service running in the other running process.
+
+The demo can also be run in a single process by running it as a normal python script using either the
+`python greetings.py` or `python -m greetings` command. This will not direct calls to an external service, everything
+runs in the same process as normal.
+"""
 from bevy import inject, dependency
 from schism import Service
 
