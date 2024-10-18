1e462e24a7b8c4789aa2b452dcc5234f772dd046
ZechCodes <hi@zech.codes>
2024-10-18 14:10:47 -0400

    Add startup message to greeting service demo

--- demos/greetings/greetings.py
+++ demos/greetings/greetings.py
@@ -5,6 +5,10 @@ remote = True
 
 
 class GreetingService(Service):
+    def __init__(self):
+        super().__init__()
+        print(f"{'[REMOTE]' if remote else '[LOCAL]'} Greeting service started")
+
     async def greet(self, name: str) -> str:
         print(f"{'[REMOTE]' if remote else '[LOCAL]'} Handling request...")
         return f"Hello, {name}!"
