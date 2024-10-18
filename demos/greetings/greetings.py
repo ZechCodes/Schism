0d5bbcb224cba2286bef5604bf29b99e5cb57505
ZechCodes <hi@zech.codes>
2024-10-18 15:49:47 -0400

    Improve and update the greeting demo

--- demos/greetings/greetings.py
+++ demos/greetings/greetings.py
@@ -4,13 +4,13 @@ Greeting Demo
 When running this script directly it will run as a single, monolithic, application in a single process.
 
 If it is run using the schism command it must be run as two separate processes: the greeting service and the "user"
-facing application. To run the greeting service use the command: `schism run services greeting-service`. Then you can
-run the application using the command: `schism run greetings.main`. You can run that any number of times and it will
-always direct calls to the greeting service class to the service running in the other running process.
+facing application. To run the greeting service use the command: `schism run service greeting`. Then you can run the
+application using the command: `schism run greetings:main`. You can run that second command any number of times and each
+time it directs calls to the greeting service running in the other running process.
 
 The demo can also be run in a single process by running it as a normal python script using either the
-`python greetings.py` or `python -m greetings` command. This will not direct calls to an external service, everything
-runs in the same process as normal.
+`python greetings.py` or `python -m greetings` command. This keeps everything running in the same process with no
+changes made in the runtime.
 """
 from bevy import inject, dependency
 from schism import Service
@@ -21,10 +21,11 @@ remote = True
 class GreetingService(Service):
     def __init__(self):
         super().__init__()
-        print(f"{'[REMOTE]' if remote else '[LOCAL]'} Greeting service started")
+        self.running = "REMOTE" if remote else "LOCAL"
+        print(f"[{self.running}] Greeting service created")
 
     async def greet(self, name: str) -> str:
-        print(f"{'[REMOTE]' if remote else '[LOCAL]'} Handling request...")
+        print(f"[{self.running}] Handling request...")
         return f"Hello, {name}!"
 
 
